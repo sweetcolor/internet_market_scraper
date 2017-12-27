6974efa38cb72eb966170956a2eac0d120d68112
Yurii Zhytskyi <sweet3color@gmail.com>
2017-12-27 15:34:15 +0200

    add config (test version)

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+
+CACHE_DIRECTORY = os.path.join(os.path.expanduser('~'), os.path.join('temp', 'cache'))
+RESULT_DIRECTORY = os.path.join(os.path.expanduser('~'), os.path.join('temp', 'result'))
+# CACHE_DIRECTORY = 'cache'
+# RESULT_DIRECTORY = 'result'
