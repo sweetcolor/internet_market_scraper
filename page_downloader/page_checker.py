4255d9f4a057e0039e86c731b76dea4b3b218099
Yurii Zhytskyi <sweet3color@gmail.com>
2018-01-02 11:50:25 +0200

    remove pagegetter

--- page_downloader/page_checker.py
+++ page_downloader/page_checker.py
@@ -5,5 +5,7 @@ class PageChecker:
     def __init__(self):
         pass
 
+    # TODO save robot answer and create this method
     def page_is_valid(self, page_source: str) -> bool:
-        pass
+        bs_page = bs4.BeautifulSoup(page_source, 'html5lib')
+        return True
